f16b6fca68191cc29396d263815ead1a3f8669fc
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-17 16:12:33 +0530

    Add initial build output for Backend project

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c18dee5d438659b8faf9bb915ee6dcdd8d8cd3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5bc9d289f7d0089b7f54365fd7bce1730b88657")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
