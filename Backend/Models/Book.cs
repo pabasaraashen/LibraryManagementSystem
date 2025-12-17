25094850d5c62b1206c6f2c1b6c5289b16516a04
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-17 10:59:04 +0530

    Update Book.cs

---
+++ Backend/Models/Book.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+namespace Backend.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
