d2afbaa479f5fbdd9cb667a3abb534cfa4ebc21d
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-17 14:27:08 +0530

    Add BookController with GET endpoint

---
+++ Backend/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+using Backend.Data;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public BookController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Book
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+    }
+}
