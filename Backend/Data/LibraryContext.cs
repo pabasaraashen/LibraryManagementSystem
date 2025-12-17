3e256e982ba9c14069ba43423e2ff7be68ea9e07
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-17 11:46:34 +0530

    update program.cs, add Sqlite package and solve the errors shows on namespace

--- Backend/Data/LibraryContext.cs
+++ Backend/Data/LibraryContext.cs
@@ -1,2 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Backend.Models;
+
+namespace Backend.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd5f35a11dbd0da4eb932b56503f804a2d0352aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa7462d7124d40ac90d56a68bf271e87bb2b92c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
