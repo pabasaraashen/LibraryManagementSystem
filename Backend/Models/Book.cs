aa7462d7124d40ac90d56a68bf271e87bb2b92c7
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-17 11:18:11 +0530

    Add EntityFrameworkCore and update Book model
    Solve the errors on packages. Added Microsoft.EntityFrameworkCore package to the project and created a basic LibraryContext file. Updated the Book model to 'required' properties for Title, Author, and Description.

--- Backend/Models/Book.cs
+++ Backend/Models/Book.cs
@@ -4,10 +4,10 @@ namespace Backend.Models
     public class Book
     {
         public int Id { get; set; }
-        [Required]
-        public string Title { get; set; }
-        [Required]
-        public string Author { get; set; }
-        public string Description { get; set; }
+        
+        public required string Title { get; set; }
+        
+        public required string Author { get; set; }
+        public required string Description { get; set; }
     }
 }
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a7101482387dc42eb351d9b6052cc868b38a47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd5f35a11dbd0da4eb932b56503f804a2d0352aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
