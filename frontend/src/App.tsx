c124a4d8564407579fde7a654758a43c3ae327d1
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-17 19:02:18 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
 import BookForm from './components/BookForm';
 import './App.css'
+import { useEffect, useState } from 'react';
+import type { Book } from './types';
+import { getBooks } from './api';
 
 function App() {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  const loadBooks = async () => {
+        try {
+            const data = await getBooks();
+            setBooks(data);
+        } catch (error) {
+            console.error("Error loading books", error);
+        }
+    };
   return (
           <div>
             <div className="app-header">
