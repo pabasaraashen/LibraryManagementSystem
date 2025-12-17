c10f2fccca167b1cbd3adfef02de41a22e1efb70
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-17 16:46:53 +0530

    Update App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  return (
+    
+            <div className="app-header">
+                <div className="title">
+                    <h3>Library Management System</h3>
+                </div>
+                
+                    <button className="primary-btn" >
+                        + Add Book
+                    </button>
+                
+            </div>
+  )
+}
+
+export default App
