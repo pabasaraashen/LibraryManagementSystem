fd5f35a11dbd0da4eb932b56503f804a2d0352aa
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-17 11:01:56 +0530

    Update Book.cs

--- Backend/Models/Book.cs
+++ Backend/Models/Book.cs
@@ -8,7 +8,6 @@ namespace Backend.Models
         public string Title { get; set; }
         [Required]
         public string Author { get; set; }
-        [Required]
         public string Description { get; set; }
     }
 }
